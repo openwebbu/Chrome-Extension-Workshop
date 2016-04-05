c70f10ced7173883ab6a4d56efc1bc6b933ac5f4
Claire W <cdwang@bu.edu>
2016-04-05 17:26:50 -0400

    replaced vanilla js

--- popup.js
+++ popup.js
@@ -1,15 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  var checkPageButton = document.getElementById('checkPage');
   var url = ''; 
       
-      checkPageButton.addEventListener('click', function() {
-  
+      $('#checkPage').on('click',function(){
       chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
         var url = tabs[0].url;
       var request = $.ajax( {
         url:'https://api.aylien.com/api/v1/summarize',
-        //headers: { 'X-TextRazor-Key': '1e858ee751d3f99127f9ab1429176f82c7a944ff5f9c5d34ff906ceb'},
+      
         headers:{'X-AYLIEN-TextAPI-Application-Key':'8968137332783762d4117e8532aff787','X-AYLIEN-TextAPI-Application-ID':'ac9f0736'},
         dataType: 'json',
         type: 'POST',
@@ -18,12 +16,11 @@ document.addEventListener('DOMContentLoaded', function() {
       });
 
       request.done(function(res) {
-         console.log(res); 
-        document.getElementById("summary").innerHTML = res.sentences[0] + res.sentences[1]; 
+        $('#summary').html(res.sentences[0] + res.sentences[1]); 
 
       
       }); 
     });
   
-  }, true);
+  });
 }, true);
